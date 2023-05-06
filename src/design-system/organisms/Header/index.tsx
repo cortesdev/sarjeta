453e494c946544756ff7dd006a74dd9b8429221b
Ricardo Côrtes <giralander@hotmail.com>
2023-05-06 16:14:31 +0200

    hero work

--- src/design-system/organisms/Header/index.tsx
+++ src/design-system/organisms/Header/index.tsx
@@ -36,7 +36,7 @@ const Header: FC<HeaderProps> = ({ pageWidth }) => {
             <StyledContainer direction="row" justifyContent="space-between" sx={{ maxHeight: 100 }}>
                 <Link href='/'>
                     <img src="/images/SVG/logo-horiz.svg"
-                        height={!scroll ? 120 : 100} alt="logo" />
+                        height={!scroll ? 120 : 70} alt="logo" />
                 </Link>
 
                 <Navbar />
